b4a377e380d9ea6bcf6b346c35678731f2dae375
cristian lobo <cristianflobo@hotmail.com>
2022-05-21 21:59:14 -0500

    navbar2

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,12 +1,19 @@
 // import axios from 'axios'
 import products from '../Products.json'
 export const GET_PRODUCTS = 'GET_PRODUCTS'
+export const SEARCH_BAR = 'SEARCH_BAR'
 
 
 
 export const getProducts = () => {
     return {
         type: GET_PRODUCTS,
+        payload: products
+    }
+}
+export const getSearchBar = () => {
+    return {
+        type: SEARCH_BAR,
         payload: products
     }
 }
