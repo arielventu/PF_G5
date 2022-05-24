54faf2f8ef5a2ebec48bc6841880002aa91fe510
evillalba510 <evillalba510@gmail.com>
2022-05-24 03:40:28 -0500

    update: mejora endpont productos

--- api/src/controllers/products.controllers.js
+++ api/src/controllers/products.controllers.js
@@ -5,13 +5,25 @@
   date: 20-05-2022  
 -----------------------------------------------*/
 
-//const { send } = require("express/lib/response");
-const { Product, Review, Stock, Category } = require("../db.js");
+const { Product, Review, Stock, Category, Sizes, Colors } = require("../db.js");
 
 const getProducts = async (req, res) => {
   try {
     const products = await Product.findAll({
-      include: [Category, Stock, Review],
+      include: [
+        { model: Category, attributes: ["name"] },
+
+        {
+          model: Stock,
+          attributes: ["quantity", "available"],
+          include: [
+            { model: Sizes, attributes: ["name"] },
+            { model: Colors, attributes: ["name"] },
+          ],
+        },
+
+        { model: Review, attributes: ["description", "starsLevel"] },
+      ],
     });
     res.json(products);
   } catch (error) {
@@ -23,7 +35,20 @@ const getProduct = async (req, res) => {
   const { id } = req.params;
   try {
     const product = await Product.findByPk(id, {
-      include: [Category, Stock, Review],
+      include: [
+        { model: Category, attributes: ["name"] },
+
+        {
+          model: Stock,
+          attributes: ["quantity", "available"],
+          include: [
+            { model: Sizes, attributes: ["name"] },
+            { model: Colors, attributes: ["name"] },
+          ],
+        },
+
+        { model: Review, attributes: ["description", "starsLevel"] },
+      ],
     });
 
     if (!product)
