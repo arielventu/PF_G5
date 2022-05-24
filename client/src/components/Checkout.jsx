05f2bce6bfbcea30ab443ea19412cf37104bdd5f
Ariel Ventura <arielventu@gmail.com>
2022-05-24 15:22:20 -0300

    show features in development

---
+++ client/src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Construction from './Construction'
+
+const Checkout = () => {
+  return (
+    <div>
+        <Construction/>
+    </div>
+  )
+}
+
+export default Checkout
