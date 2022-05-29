134d0a2cbe9c75647b19800eed594dc2d0f72420
Ariel Ventura <arielventu@gmail.com>
2022-05-29 00:16:21 -0300

    pre deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import App from './App';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
-import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 const rootElement = document.getElementById("root");
