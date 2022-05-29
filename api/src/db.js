a4eafb19e84045eec8adef170c501051c83e0c5d
Ariel Ventura <arielventu@gmail.com>
2022-05-28 23:40:51 -0300

    pre deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
