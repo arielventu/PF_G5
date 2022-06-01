1871dfc6fd8ae5691be637578989fcf3e19f0cbe
Ariel Ventura <arielventu@gmail.com>
2022-06-01 16:02:11 -0300

    A

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react'
-import { Star } from 'react-star';
-import styles from './Reviews.modules.css'
+import React, { useState } from "react";
+import styles from "./Reviews.modules.css";
 
 const Reviews = () => {
-  
-    // const showReviewText = (text) => {
-    //     if (text.length > 100) {
-    //         return text.substring(0, 100) + "..."
-    //     }
-    //     return text
-    // }
-    var show = false;
-    const showRevText = () => {
-        show = !show
-    }
-    return (
-        <div>
-        <Star className={styles.star}
-                // onChange={(value) => console.log(value)}
-                onClick={showRevText}
-        />
-            {show &&
-                <form>
+  var show = false;
+  const showRevText = () => {
+    show = !show;
+  };
+  return (
+    <div>
+      <div class="cont">
+        <div class="stars">
+          <form action="">
+            <input class="star star-5" id="star-5-2" type="radio" name="star" />
+            <label class="star star-5" for="star-5-2"></label>
+            <input class="star star-4" id="star-4-2" type="radio" name="star" />
+            <label class="star star-4" for="star-4-2"></label>
+            <input class="star star-3" id="star-3-2" type="radio" name="star" />
+            <label class="star star-3" for="star-3-2"></label>
+            <input class="star star-2" id="star-2-2" type="radio" name="star" />
+            <label class="star star-2" for="star-2-2"></label>
+            <input class="star star-1" id="star-1-2" type="radio" name="star" />
+            <label class="star star-1" for="star-1-2"></label>
+            <div class="rev-box">
+              <textarea class="review" col="30" name="review"></textarea>
+              <label class="review" for="review">
+                Breif Review
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* <form>
             <div className={styles.revBox}>
                 <textarea className={styles.textAreaReview} col="30" name="review"></textarea>
                 <label className={styles.labelReview} for="review">Breif Review</label>
             </div>
-        </form>}
+        </form> */}
     </div>
-    );
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
