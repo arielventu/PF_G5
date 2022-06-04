562f78a8d65fafe20e47337eaf8a7c14d2b42dfc
Ariel Ventura <arielventu@gmail.com>
2022-06-04 16:52:41 -0300

    review component 80%

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getReviews, getReviewsById, setReviews } from "../actions/actions";
+import starB from "../image/starb.svg";
+import starY from "../image/stary.svg";
 import styles from "./Reviews.module.css";
 import swal from "sweetalert";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@htmlFortawesome/react-fontawesome";
 // import {
 //     faStar
-// } from "@fortawesome/free-solid-svg-icons";
+// } from "@htmlFortawesome/free-solid-svg-icons";
 
 const Reviews = () => {
   // const { productId } = useParams();
@@ -16,16 +18,18 @@ const Reviews = () => {
   const reviews = useSelector((state) => state.allReviews);
   const reviewsById = useSelector((state) => state.reviewsById);
   //   const [reviewsList, setReviewsList] = useState([]);
-  const [newReview, setNewReview] = useState({
+  const initialReview = {
+    username: "",
     description: "",
-    starLevel: 0,
+    starsLevel: 0,
     productId: productId,
-  });
+  }
+    const [newReview, setNewReview] = useState(initialReview);
 
   useEffect(() => {
     dispatch(getReviews());
     dispatch(getReviewsById(productId));
-    console.log(reviews);
+    // console.log(reviews);
   }, []);
 
   //   useEffect(() => {
@@ -33,101 +37,118 @@ const Reviews = () => {
   //   }, [reviews]);
 
   const handleChange = (e) => {
-    // const { name, value } = e.target;
-    // setReviewsList({
-    //   ...reviewsList,
-    //   [name]: value,
-    // });
-    console.log(e.target.value);
+    setNewReview({
+      ...newReview,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(setReviews(newReview));
-    swal("Success!", "Your review has been added!", "success");
-    setNewReview({
-      description: "",
-      starLevel: 0,
-      productId: productId,
-    });
-    reviews.push(newReview);
+    if (newReview.starsLevel === 0) {
+        swal({
+            text: "Please don't forget to rate us with stars",
+            icon: "warning",
+            buttons: false,
+            timer: 3000,
+        });
+    } else if (newReview.description.length === 0) {
+        swal({
+            text: "Please don't forget to write a review",
+            icon: "warning",
+            buttons: false,
+            timer: 3000,
+        });
+    } else {
+          dispatch(setReviews(newReview));
+          swal("Success!", "Your review has been added!", "success");
+          setNewReview(initialReview);
+          reviews.push(newReview);
+      }
   };
 
   return (
-    <div>
-      <div className={styles.reviews}>
-        <div className={styles.reviews__header}>
-          <h2>Reviews</h2>
-        </div>
-
-        <div className={styles.reviews__body__reviews__review__form}>
-          <form onSubmit={handleSubmit}>
-                      <div className={styles.reviews__body__reviews__review__form__stars}>
-                          <div className={styles.reviews__body__reviews__review__form__stars__star}>
-                              <input type="radio" name="starLevel" value="1" onChange={handleChange} />
-                              <label htmlFor="star1">1</label>
-                          </div>
-                          <div className={styles.reviews__body__reviews__review__form__stars__star}>
-                              <input type="radio" name="starLevel" value="2" onChange={handleChange} />
-                              <label htmlFor="star2">2</label>
-                          </div>
-                          <div className={styles.reviews__body__reviews__review__form__stars__star}>
-                              <input type="radio" name="starLevel" value="3" onChange={handleChange} />
-                              <label htmlFor="star3">3</label>
-                          </div>
-                          <div className={styles.reviews__body__reviews__review__form__stars__star}>
-                              <input type="radio" name="starLevel" value="4" onChange={handleChange} />
-                              <label htmlFor="star4">4</label>
-                          </div>
-                          <div className={styles.reviews__body__reviews__review__form__stars__star}>
-                              <input type="radio" name="starLevel" value="5" onChange={handleChange} />
-                              <label htmlFor="star5">5</label>
-                          </div>
-                      </div>
-                      <div className={styles.reviews__body__reviews__review__form__description}>
-                          <textarea name="description" id="description" cols="30" rows="10" onChange={handleChange}></textarea>
-                      </div>
-                      <div className={styles.reviews__body__reviews__review__form__submit}>
-                          <input type="submit" value="Submit" />
-                      </div>
-                  </form>
-              </div>
+      <div className={styles.divContainer}>
+        <div className={styles.divInnerContainer}>
+            <div className={styles.divReviewsHeader}>
+            <h2 className={styles.reviewsHeader}>Thank for your review!</h2>
+            </div>
+            <div className={styles.divReviewFormContainer}>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.divStarsContainer}>
+                    <div className={styles.divStar}>
+                        <input className={styles.inputStar} id="sl1" type="radio" name="starsLevel" value="1" onChange={handleChange}/>
+                        <label className={styles.labelStar1} htmlFor="sl1">
+                            {newReview.starsLevel < 1 ?
+                                <img className={styles.star} src={starB} alt="star" />
+                                : <img className={styles.star} src={starY} alt="star" />
+                            }
+                        </label>
+                    </div>
+                    <div className={styles.divStar}>
+                        <input className={styles.inputStar} id="sl2" type="radio" name="starsLevel" value="2" onChange={handleChange}/>
+                        <label className={styles.labelStar2} htmlFor="sl2">
+                            {newReview.starsLevel < 2 ?
+                                <img className={styles.star} src={starB} alt="star" />
+                                : <img className={styles.star} src={starY} alt="star" />
+                            }
+                        </label>
+                    </div>
+                        <div className={styles.divStar}>
+                        <input className={styles.inputStar} id="sl3" type="radio" name="starsLevel" value="3" onChange={handleChange}/>
+                        <label className={styles.labelStar3} htmlFor="sl3">
+                            {newReview.starsLevel < 3 ?
+                                <img className={styles.star} src={starB} alt="star" />
+                                : <img className={styles.star} src={starY} alt="star" />
+                            }
+                        </label>
+                    </div>
+                    <div className={styles.divStar}>
+                        <input className={styles.inputStar} id="sl4" type="radio" name="starsLevel" value="4" onChange={handleChange}/>
+                        <label className={styles.labelStar4} htmlFor="sl4">
+                            {newReview.starsLevel < 4 ?
+                                <img className={styles.star} src={starB} alt="star" />
+                                : <img className={styles.star} src={starY} alt="star" />
+                            }
+                        </label>
+                    </div>
+                    <div className={styles.divStar}>
+                        <input className={styles.inputStar} id="sl5" type="radio" name="starsLevel" value="5" onChange={handleChange}/>
+                        <label className={styles.labelStar5} htmlFor="sl5">
+                            {newReview.starsLevel < 5 ?
+                                <img className={styles.star} src={starB} alt="star" />
+                                : <img className={styles.star} src={starY} alt="star" />
+                            }
+                        </label>
+                    </div>
+                    </div>
+                    {/* <div className={styles.divReviewsHeader}>
+                        <h2>Your name</h2>
+                    </div>
+                    <div className={styles.divUsernameReview}>
+                        <input  className={styles.usernameReview} type="text" cols="30" name="username" id="username" onChange={handleChange}/>
+                    </div> */}
+                    {/* <div className={styles.divDescriptionTitle}>
+                        <h2 className={styles.descriptionTitle}>Your review</h2>
+                    </div> */}
+                    <div className={styles.divDescriptionReview}>
+                          <textarea className={styles.textareaReview}
+                              placeholder="Write your review here"
+                              name="description"
+                              id="description"
+                              cols="30"
+                              rows="6"
+                              onChange={handleChange}>
+                            </textarea>
+                    </div>
+                    <div className={styles.divSubmitReview}>
+                    <input className={styles.submitReview} type="submit" value="Submit" />
+                    </div>
+            </form>
+            </div>
           </div>
-      </div>
+        </div>
   );
 };
-
-//          <div class="stars">
-//            <form action="">
-//              <input class="star star-5" id="star-5-2" type="radio" name="star" />
-//              <label class="star star-5" for="star-5-2"></label>
-//              <input class="star star-4" id="star-4-2" type="radio" name="star" />
-//              <label class="star star-4" for="star-4-2"></label>
-//              <input class="star star-3" id="star-3-2" type="radio" name="star" />
-//              <label class="star star-3" for="star-3-2"></label>
-//              <input class="star star-2" id="star-2-2" type="radio" name="star" />
-//              <label class="star star-2" for="star-2-2"></label>
-//              <input class="star star-1" id="star-1-2" type="radio" name="star" />
-//              <label class="star star-1" for="star-1-2"></label>
-//              <div class="rev-box">
-//                <textarea class="review" col="30" name="review"></textarea>
-//                <label class="review" for="review">
-//                  Breif Review
-//                </label>
-//                            <button class="submit" type="submit">Submit</button>
-//              </div>
-//            </form>
-//          </div>
-//        </div>
-
-//         <form>
-//             <div className={styles.revBox}>
-//                 <textarea className={styles.textAreaReview} col="30" name="review"></textarea>
-//                 <label className={styles.labelReview} for="review">Breif Review</label>
-//             </div>
-//         </form>
-//     </div>
-//    );
-// };
 
 export default Reviews;
