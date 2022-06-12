ec7868f5b33586396c796ee66ce45f2a2088a90c
javier ferrari <javierferrarimezher@gmail.com>
2022-06-12 16:07:56 -0300

    merging from dev

--- api/src/controllers/mail.controllers.js
+++ api/src/controllers/mail.controllers.js
@@ -12,34 +12,77 @@ var transporter = nodemailer.createTransport({
 const sendEmail = async (req, res) => {
     const { amount, shippingAddress, orderEmail, orderDate, orderStatus, image } =
     req.body;
+    if(orderStatus !== "completed" && orderStatus !== "dispatched") {
 
-    const contentHTML = `
-    <h1>Thank you for your Purchase</h1>
-    <p>Detail:</p>
-    <img src=${image} alt='not found'><img>
-    <ul>
-    <li> Username: ${orderEmail}</li>
-    <li> Amount: ${amount}</li>
-    </ul>
-    `;
-
-    var mailOptions = {
-        from: 'bluebirdcommerce@gmail.com',
-        to: orderEmail,
-        subject: 'Sending Email using Node.js',
-        html: contentHTML
+        res.send("orderStatus no esta completada o despachada");
     }
-    ;
+    
+    
+    if(orderStatus === "completed") {
+        
+        const contentHTML = `
+        <h1>Thank you for your Purchase</h1>
+        <p>Detail:</p>
+        <img width="200px" height="200px" src=${image} alt='not found'><img>
+        <ul>
+        <li> Username: ${orderEmail}</li>
+        <li> Amount: ${amount}</li>
+        </ul>
+        `;
 
-    await transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-    });
+       
+    
+        var mailOptions = {
+            from: 'bluebirdcommerce@gmail.com',
+            to: orderEmail,
+            subject: 'Sending Email using Node.js',
+            html: contentHTML
+        }
+        ;
+    
+        await transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+    
+        res.send("mail enviado estado completed");
 
-    res.send("mail enviado");
+    } 
+    
+    if (orderStatus === "dispatched") {
+        
+        const contentHTML = `
+        <h1>Your order has been dispatched!</h1>
+        <p>Detail:</p>
+        <img src=${image} alt='not found'><img>
+        <ul>
+        <li> Username: ${orderEmail}</li>
+        <li> Amount: ${amount}</li>
+        </ul>
+        `;
+    
+        var mailOptions = {
+            from: 'bluebirdcommerce@gmail.com',
+            to: orderEmail,
+            subject: 'Sending Email using Node.js',
+            html: contentHTML
+        }
+        ;
+    
+        await transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+    
+        res.send("mail enviado estado dispatched");
+
+    } 
 }
 
 module.exports = {
